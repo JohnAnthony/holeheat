2cba5d37fe6d05fa4d077ababbe56b771741b0ee
John Anthony <john@jo.hnanthony.com>
2017-07-15 21:42:00 +0100

    First working yield estimation (!)

--- api/server.js
+++ api/server.js
@@ -1,3 +1,4 @@
+var _ = require('underscore');
 var express = require('express');
 var router = express.Router();
 
@@ -49,13 +50,62 @@ app.post('/borehole/:id', TODO);
 
 app.delete('/borehole/:id', TODO);
 
-app.get('/borehole/:id/yield', TODO);
+app.get('/borehole/:id/yield/:length/:hours', function(req, res) {
+	var length = Number(req.params.length);
+	var hours = Number(req.params.hours);
+
+	if ([1800, 2400].indexOf(hours) == -1) {
+		res.status(500);
+		res.send('Invalid hours value');
+	}
+
+	if (length <= 0) {
+		res.status(500);
+		res.send('Invalid wellbore length');
+	}
+
+	Borehole.findOne({ _id: req.params.id }, function(err, b) {
+		if (err) {
+			res.status(500);
+			res.send(err);
+		}
+
+		var yield = 0;
+		var key = 'h' + hours;
+
+		var promises = _.map(b.strata, function(s) {
+			return Formation.findOne({ _id: s.formation	});
+		});
+		Promise.all(promises).then(function(formations) {
+			_.each(formations, function(e, i) {
+				if (length <= 0)
+					return;
+
+				var yield_set = e[key];
+				var mean_yield = (yield_set.min + yield_set.max) / 2;
+				var l = Math.min(b.strata[i].depth, length);
+
+				length -= l;
+				yield += mean_yield * l;
+			})
+			res.send(String((yield * hours) / 1000));
+		});
+	});
+});
 
 app.get('/borehole/search/:north/:east', TODO);
 
 app.put('/formation', TODO);
 
-app.get('/formation', TODO);
+app.get('/formation', function(req, res) {
+	Formation.find(function(err, formations) {
+		if (err) {
+			res.status(500);
+			res.send(err);
+		}
+		res.json(formations);
+	});
+});
 
 app.put('/formation/:id', TODO);
 
